dc7373e55b21bf93e0b63b11bfad34163158922f
Jean-Bernard Rault <jean-bernard.rault@b-com.com>
2018-02-26 15:00:34 +0100

    add 'const' to constant parameters

--- interfaces/api/solver/map/ITriangulator.h
+++ interfaces/api/solver/map/ITriangulator.h
@@ -24,12 +24,12 @@ public:
     virtual ~ITriangulator() = default;
 
     /// @brief triangulate pairs of points 2d captured from two views with differents poses (with respect to the camera instrinsic parameters).
-    virtual bool triangulate(std::vector<SRef<Point2Df>>& pt2d_1,
-                             std::vector<SRef<Point2Df>>& pt2d_2,
-                             SRef<Pose>&p1,
-                             SRef<Pose>&p2,
-                             CamCalibration&cam,
-                             CamDistortion&dist,
+    virtual bool triangulate(const std::vector<SRef<Point2Df>>& pt2d_1,
+                             const std::vector<SRef<Point2Df>>& pt2d_2,
+                             const SRef<Pose>&p1,
+                             const SRef<Pose>&p2,
+                             const CamCalibration&cam,
+                             const CamDistortion&dist,
                              std::vector<SRef<Point3Df>>& pt3d)=0;
 
     XPCF_DECLARE_UUID("3a01b0e9-9a76-43f5-97b3-85bb6979b953");
