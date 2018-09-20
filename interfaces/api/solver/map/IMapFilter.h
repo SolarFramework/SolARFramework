300e962565a5e6d45af741d9f4a21e73a07d440b
BCOM\jroyan <jerome.royan@b-com.com>
2018-09-20 16:07:35 +0200

    Change the interface of the mapfilter to simplify it

--- interfaces/api/solver/map/IMapFilter.h
+++ interfaces/api/solver/map/IMapFilter.h
@@ -11,7 +11,7 @@
 #include "xpcf/api/IComponentIntrospect.h"
 #include "core/Messages.h"
 #include "datastructure/GeometryDefinitions.h"
-
+#include "datastructure/Keyframe.h"
 #include "datastructure/CloudPoint.h"
 
 namespace SolAR {
@@ -22,32 +22,25 @@ namespace SolAR {
 ///
 /// @brief The IMapFilter class
 ///
-            class  IMapFilter : public virtual org::bcom::xpcf::IComponentIntrospect {
-                    public:
-                       IMapFilter() = default;
-                       ///
-                       ///@brief ~IMapper
-                       ///
-                       virtual ~IMapFilter() {}
+class  IMapFilter : public virtual org::bcom::xpcf::IComponentIntrospect {
+        public:
+           IMapFilter() = default;
+           ///
+           ///@brief ~IMapper
+           ///
+           virtual ~IMapFilter() {}
 
+           /// @brief  Filter point cloud reconstructed from 2 viewpoints
+           /// @param[in] view1: the first keyframe used for building the point cloud.
+           /// @param[in] view2: the second keyframe used for building the point cloud.
+           /// @param[in] Set of triangulated 3d_points..
+           /// @param[out] filtered point cloud
+           virtual void  filter(const SRef<Keyframe> view1, const SRef<Keyframe> view2, const std::vector<SRef<CloudPoint>>& input,  std::vector<SRef<CloudPoint>>& output) = 0;
+        };
+}
 
-					   /// @brief Check triangulation status.Warn negative-Z triangulated points.
-					   /// @param[in] Set of triangulated 3d_points.
-					   /// @param[in] Camera pose of the second view at triangulation step (the first one supposed canonical).
-					   /// @param[out] Status of each reprojected =3d_point (false: negative-z, true: non-negative z).
-					   /// @return validity of the triangulated points
-					   virtual  bool checkFrontCameraPoints(const std::vector<SRef<CloudPoint>>& pcloud, const Transform3Df & cameraPose, std::vector<bool> & isFrontCamera) = 0;
-
-					   /// @brief  Filter point cloud according to reprojection error and front camera status
-					   /// @param[in] Set of triangulated 3d_points.
-					   /// @param[in] Status of each reprojected =3d_point (false: negative-z, true: non-negative z).
-					   /// @param[out] filtered point cloud without z negative points and points with a large reprojection error
-                       virtual void  filterPointCloud(const std::vector<SRef<CloudPoint>>& input, const std::vector<bool> & isFrontCamera,  std::vector<SRef<CloudPoint>>& output) = 0;
-
-                    };
-            }
-        }
-    }
+}
+}
 }
 XPCF_DEFINE_INTERFACE_TRAITS(SolAR::api::solver::map::IMapFilter,
                              "e0d6cc82-6af2-493d-901a-2384fca0b16f",
