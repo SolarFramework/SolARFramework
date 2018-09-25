2ffaaf42636c88ef11e5db0eedb408c67eb138a6
BCOM\jroyan <jerome.royan@b-com.com>
2018-09-25 13:24:09 +0200

    Remove unused method Compute Gravity

--- src/datastructure/Map.cpp
+++ src/datastructure/Map.cpp
@@ -26,30 +26,5 @@ SRef<std::vector<SRef<CloudPoint>>> Map::getPointCloud()
 
 }
 
-void Map::computeGravity(Point3Df &grav, float& maxDist) const
-{
-   grav =  Point3Df(0, 0, 0);
-   maxDist  =0.f;
-
-   Point3Df  vr_temp(0, 0, 0);
-    int count = 0;
-    for (int i = 0; i < m_pointCloud->size(); ++i) {
-        vr_temp = Point3Df((*m_pointCloud)[i]->getX(), (*m_pointCloud)[i]->getX(), (*m_pointCloud)[i]->getY());
-        grav = grav + vr_temp;
-        count++;
-    }
-    maxDist = 0;
-    if (count > 0) {
-        grav= grav*(1.f / (float)count);
-        for (int i = 0; i < m_pointCloud->size(); ++i) {
-            vr_temp = Point3Df((*m_pointCloud)[i]->getX(),(*m_pointCloud)[i]->getY(), (*m_pointCloud)[i]->getZ());
-            Point3Df temp = vr_temp - grav;
-            maxDist = std::max(maxDist, (vr_temp - grav).magnitude());
-        }
-    }
-}
-
-
-
 }
 }
