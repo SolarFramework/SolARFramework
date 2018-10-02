922fa55a9fb5a6c9cfd6fbcc2d73d6286258268d
BCOM\jroyan <jerome.royan@b-com.com>
2018-10-02 10:39:38 +0200

    Add a new interface to ITriangulator

--- interfaces/api/solver/map/ITriangulator.h
+++ interfaces/api/solver/map/ITriangulator.h
@@ -23,11 +23,7 @@
 
 #include "xpcf/api/IComponentIntrospect.h"
 #include "core/Messages.h"
-#include "datastructure/GeometryDefinitions.h"
-#include "datastructure/MathDefinitions.h"
-#include "datastructure/Image.h"
-#include "datastructure/Keypoint.h"
-#include "datastructure/CloudPoint.h"
+#include "datastructure/Keyframe.h"
 #include "datastructure/DescriptorMatch.h"
 
 
@@ -87,6 +83,16 @@ public:
                                const Transform3Df& poseView1,
                                const Transform3Df& poseView2,
                                std::vector<SRef<CloudPoint>>& pcloud)=0;
+
+    /// @brief triangulate pairs of points 2d captured from current keyframe with its reference keyframe using their poses (with respect to the camera instrinsic parameters).
+    /// @param[in] curKeyframe, current keyframe.
+    /// @param[in] matches, the matches between the keypoints of the view1 and the keypoints of the view 2.
+    /// @param[out] pcloud, Set of triangulated 3d_points.
+    /// @return the mean re-projection error (mean distance in pixels between the original 2D points and the projection of the reconstructed 3D points)
+    virtual double triangulate(	const SRef<Keyframe> &curKeyframe,
+                                const std::vector<DescriptorMatch>&matches,
+                                std::vector<SRef<CloudPoint>>& pcloud)=0;
+
 };
 
 }
