89f8b86b5a569effc9d54281831eb421ae6a619c
BCOM\jroyan <jerome.royan@b-com.com>
2018-12-19 23:16:47 +0100

    Extend display method to visualize PointCloud data structure

--- interfaces/api/display/I3DPointsViewer.h
+++ interfaces/api/display/I3DPointsViewer.h
@@ -20,6 +20,7 @@
 #include "xpcf/api/IComponentIntrospect.h"
 #include "core/Messages.h"
 #include "datastructure/CloudPoint.h"
+#include "datastructure/PointCloud.h"
 #include "datastructure/GeometryDefinitions.h"
 
 namespace SolAR {
@@ -53,6 +54,21 @@ public:
                                         const std::vector<Transform3Df> keyframePoses = {},
                                         const std::vector<Transform3Df> framePoses = {},
                                         const std::vector<SRef<CloudPoint>>& points2 = {},
+                                        const std::vector<Transform3Df> keyframePoses2 = {}) = 0;
+
+    /// @brief Display in a windows the 3D point cloud as well as the current camera, and optionnally, the previous frames and keyframes.
+    /// @param[in] points, Set of 3D points to display in the 3D viewer.
+    /// @param[in] pose, poses of the current camera (transform of the camera defined in world corrdinate system).
+    /// @param[in] keyframesPoses (optional), poses of a set of keyframes (transform of the camera defined in world corrdinate system).
+    /// @param[in] framePoses (optional), poses of a set of frames (transform of the camera defined in world corrdinate system).
+    /// @param[in] points2 (optional), a second set of 3D points to display in the 3D viewer (useful to visualize result of a bundle adjustment).
+    /// @param[in] keyframesPoses2 (optional), a second set of keyframes poses (transform of the camera defined in world corrdinate system, useful to visualize result of a bundle adjustment).
+    /// @return FrameworkReturnCode::_SUCCESS if the window is created, else FrameworkReturnCode::_ERROR_
+    virtual FrameworkReturnCode display(const SRef<PointCloud> points,
+                                        const Transform3Df & pose,
+                                        const std::vector<Transform3Df> keyframePoses = {},
+                                        const std::vector<Transform3Df> framePoses = {},
+                                        const SRef<PointCloud> points2 = nullptr,
                                         const std::vector<Transform3Df> keyframePoses2 = {}) = 0;
 
 
