b5c2e4a2127db91072e7876cdb22bfca6a30a7b3
Loïc TOURAINE <loic.touraine@b-com.com>
2019-09-02 15:08:09 +0200

    Extend interface: - bool select(const SRef<Frame> & frame): allow inner use of SolAR Storage components (through injection for instance) to determine if the provided frame can be selected as a keyframe - bool select(const SRef<Frame> & frame, const std::function<bool(const SRef<Frame> &)> & func): use of lamda function as predicate to determine if the frame is a candidate for a new keyframe

--- interfaces/api/solver/map/IKeyframeSelector.h
+++ interfaces/api/solver/map/IKeyframeSelector.h
@@ -53,6 +53,21 @@ public:
     /// @param[in] matches: the matches between the frame and its keyframe of reference.
     /// @return true if the frame can be considered as a new keyframe, false otherwise.
     virtual bool select(const SRef<Frame> & frame, const std::vector<DescriptorMatch> & matches) = 0;
+
+    /// @brief  Select if a frame can be considered as a keyframe
+    /// @param[in] frame: the frame tested to know if it could be a Keyframe
+    /// The underlying component can use data from SolAR data storage components, based on the frame properties.
+    /// @return true if the frame can be considered as a new keyframe, false otherwise.
+    virtual bool select(const SRef<Frame> & frame) = 0;
+
+    /// @brief  Select if a frame can be considered as a keyframe.
+    /// It is based on a selection predicate and provides the mean to use any datastructure in the pipeline context to the decision algorithm.
+    /// @param[in] frame: the frame tested to know if it could be a Keyframe
+    /// @param[in] func: the function predicate used to test the frame.
+    /// This predicate can be any lambda capturing its context (matches, point cloud, bow ...) to select the frame.
+    /// @return true if the frame can be considered as a new keyframe, false otherwise.
+   virtual bool select(const SRef<Frame> & frame, const std::function<bool(const SRef<Frame> &)> & func) = 0;
+
 };
 }
 
