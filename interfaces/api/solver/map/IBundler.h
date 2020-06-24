015f002748e93f9900a33123a3f47f96193253ad
BCOM\nduong <nam-duong.duong@b-com.com>
2020-06-24 15:57:05 +0200

    Move useSpanningTree parameter to config file

--- interfaces/api/solver/map/IBundler.h
+++ interfaces/api/solver/map/IBundler.h
@@ -27,25 +27,23 @@ namespace map {
   * <TT>UUID: 35b9bdb7-d23c-4909-984f-ae7f9a292e6c</TT>
   */
 
-class  IBundler : virtual public org::bcom::xpcf::IComponentIntrospect {
-        public:
-           IBundler() = default;
-           ///
-           ///@brief ~IBundler
-           ///
-           virtual ~IBundler() {}
+	class  IBundler : virtual public org::bcom::xpcf::IComponentIntrospect {
+	public:
+		IBundler() = default;
+		///
+		///@brief ~IBundler
+		///
+		virtual ~IBundler() {}
 
-           /// @brief solve a non-linear problem related to bundle adjustement statement expressed as:
-           /// minArg(pts3ds,intrinsics,extrinsics) = MIN_cam_i(MIN_3d_j(pts2d_j - reproje(pt3ds_j,intrinsics_i,extrinsics_i)),
-           /// @param[in, out] K: camera calibration parameters responsible of 3D points generation.
-           /// @param[in, out] D: camera distorsion parameters responsible of 3D points generation
-           /// @param[in] selectKeyframes : selected views to bundle following a given strategies. If it is empty then take all keyframes into account to perform global bundle adjustment.
-		   /// @param[in] useSpanningTree: in the case of the global bundle adjustment, if it true, the optimization is based on a maximal spanning tree.
-           /// @return the mean re-projection error after optimization.
-           virtual  double bundleAdjustment(CamCalibration & K,
-                                            CamDistortion & D,
-                                            const std::vector<uint32_t> & selectKeyframes,
-											const bool & useSpanningTree = false) = 0;
+		/// @brief solve a non-linear problem related to bundle adjustement statement expressed as:
+		/// minArg(pts3ds,intrinsics,extrinsics) = MIN_cam_i(MIN_3d_j(pts2d_j - reproje(pt3ds_j,intrinsics_i,extrinsics_i)),
+		/// @param[in, out] K: camera calibration parameters responsible of 3D points generation.
+		/// @param[in, out] D: camera distorsion parameters responsible of 3D points generation
+		/// @param[in] selectKeyframes : selected views to bundle following a given strategies. If it is empty then take all keyframes into account to perform global bundle adjustment.
+		/// @return the mean re-projection error after optimization.
+		virtual  double bundleAdjustment(CamCalibration & K,
+										CamDistortion & D,
+										const std::vector<uint32_t> & selectKeyframes = {}) = 0;
 };
 }
 }
