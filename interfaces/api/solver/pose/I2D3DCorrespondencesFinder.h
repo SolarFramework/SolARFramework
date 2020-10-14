db866e468a20d6a8428f3eb7905b43e7b8223896
BCOM\nduong <nam-duong.duong@b-com.com>
2020-10-14 15:25:20 +0200

    Fix: missing pass by reference

--- interfaces/api/solver/pose/I2D3DCorrespondencesFinder.h
+++ interfaces/api/solver/pose/I2D3DCorrespondencesFinder.h
@@ -57,12 +57,12 @@ namespace pose {
         /// @param[out] corres2D3D: The pairs of 2D-3D correspondences. The first value is the index of keypoint in the current frame and the second one is the corresponding cloud point.
         /// @param[out] found_matches: The matches between the current frame and its reference keyframe which have a 3 correspondant.
         /// @param[out] remaining_matches: The matches between the current frame and its reference keyframe for which no 3D points have been found.
-        virtual FrameworkReturnCode find(	const SRef<Frame> lastFrame,
-                                            const SRef<Frame> currentFrame,
+        virtual FrameworkReturnCode find(	const SRef<Frame> &lastFrame,
+                                            const SRef<Frame> &currentFrame,
                                             const std::vector<DescriptorMatch> & current_matches,
                                             std::vector<Point3Df> & shared_3dpoint,
                                             std::vector<Point2Df> & shared_2dpoint,
-											std::vector<std::pair<uint32_t, SRef<CloudPoint>>> corres2D3D,		
+											std::vector<std::pair<uint32_t, SRef<CloudPoint>>> &corres2D3D,		
                                             std::vector<DescriptorMatch> & found_matches,
                                             std::vector<DescriptorMatch> & remaining_matches) = 0;
 
