ed74419bbdeff7c615d966b2813e49a804c6a979
BCOM\nduong <nam-duong.duong@b-com.com>
2020-10-19 15:08:40 +0200

    Add: MapFusion interface. Update: 3DTransform interface

--- interfaces/api/solver/map/IMapFusion.h
+++ interfaces/api/solver/map/IMapFusion.h
@@ -46,13 +46,13 @@ public:
 	virtual ~IMapFusion() = default;
 
 	/// @brief Merge a map in the global map. The map can be a local map (know transformation to the global map) or a floating map.
-	/// @param[in] map: local map or floating map to merge
+	/// @param[in,out] map: local map or floating map to merge
 	/// @param[in,out] globalMap: the global map
 	/// @param[in,out] transform: the transformation to the global map (null for floating map). It can be refined by fusion process.
 	/// @param[out] nbMatches: the number of matched cloud points.
 	/// @param[error] error: the error of fusion process that is the mean of error distances of the matched cloud points.
 	/// @return FrameworkReturnCode::_SUCCESS_ if the fusion succeed, else FrameworkReturnCode::_ERROR.
-	virtual FrameworkReturnCode merge(const SRef<IMapper> &map, SRef<IMapper> &globalMap, Transform3Df &transform, uint32_t &nbMatches, float &error) = 0;
+	virtual FrameworkReturnCode merge(SRef<IMapper> &map, SRef<IMapper> &globalMap, Transform3Df &transform, uint32_t &nbMatches, float &error) = 0;
 };
 
 }
