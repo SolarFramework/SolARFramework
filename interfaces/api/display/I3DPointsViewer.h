09bae8d4bee172eda095b9bc5b678a12a806b2fa
Maxime PAPIN <maxime.papin@b-com.com>
2020-12-11 11:21:44 +0100

    fix: Update I3DPointsViewer methods signatures

--- interfaces/api/display/I3DPointsViewer.h
+++ interfaces/api/display/I3DPointsViewer.h
@@ -80,10 +80,10 @@ public:
 	/// @param[in] poses2 (optional), poses of a set of keyframes (transform of the camera defined in world coordinate system).
 	/// @param[in] lines2 (optional), a second set of 3D lines to display in the 3D viewer (useful to visualize result of a bundle adjustment).
 	/// @return FrameworkReturnCode::_SUCCESS if the window is created, else FrameworkReturnCode::_ERROR_
-	virtual FrameworkReturnCode display(const std::vector<CloudLine> & lines,
+	virtual FrameworkReturnCode display(const std::vector<SRef<CloudLine>> lines,
 										const Transform3Df & pose,
 										const std::vector<Transform3Df> & poses2 = {},
-										const std::vector<CloudLine> & lines2 = {}) = 0;
+										const std::vector<SRef<CloudLine>> lines2 = {}) = 0;
 
 	/// @brief Display in a windows the 3D point & line cloud as well as the current camera, and optionally, a second point & line cloud, other camera poses and two sets of keyframes.
 	/// @param[in] points, Set of 3D points to display in the 3D viewer.
@@ -95,14 +95,14 @@ public:
 	/// @param[in] poses2 (optional), poses from another set of camera poses (transform of the camera defined in world coordinate system).
 	/// @param[in] keyframesPoses2 (optional), a second set of keyframes poses (transform of the camera defined in world coordinate system).
 	/// @return FrameworkReturnCode::_SUCCESS if the window is created, else FrameworkReturnCode::_ERROR_
-	virtual FrameworkReturnCode display(const std::vector<CloudPoint> & points,
-										const std::vector<CloudLine> & lines,
+	virtual FrameworkReturnCode display(const std::vector<SRef<CloudPoint>> points,
+										const std::vector<SRef<CloudLine>> lines,
 										const Transform3Df & pose,
 										const std::vector<Transform3Df>& keyframePoses = {},
-										const std::vector<CloudPoint> & points2 = {},
-										const std::vector<CloudLine> & lines2 = {},
+										const std::vector<SRef<CloudPoint>> points2 = {},
+										const std::vector<SRef<CloudLine>> lines2 = {},
 										const std::vector<Transform3Df> & poses2 = {},
-										const std::vector<Transform3Df>& keyframePoses2 = {}) = 0;
+										const std::vector<Transform3Df> & keyframePoses2 = {}) = 0;
 };
 }
 }
