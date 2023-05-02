ac9144c820395a330ef62321a4685137b0dc9b65
yzhou35 <Yitian.Zhou@b-com.com>
2023-05-02 15:39:52 +0200

    Modify code according to PR comments

--- interfaces/datastructure/Frame.h
+++ interfaces/datastructure/Frame.h
@@ -92,8 +92,8 @@ public:
     void setKeypoints(const std::vector<Keypoint>& kpts);
 	
     /// @brief update keypoint class id
-    /// @param[in] i: index of keypoint
-    /// @param[in] classId: semantic class Id of this keypoint
+    /// @param[in] i index of keypoint
+    /// @param[in] classId semantic class Id of this keypoint
     /// @return true if update class id successfully
     bool updateKeypointClassId(int i, int classId);
 
--- src/datastructure/Frame.cpp
+++ src/datastructure/Frame.cpp
@@ -77,8 +77,10 @@ void Frame::setKeypoints(const std::vector<Keypoint> & kpts){
 
 bool Frame::updateKeypointClassId(int i, int classId) 
 {
-    if (i < 0 || i >= static_cast<int>(m_keypoints.size()) || i >= static_cast<int>(m_keypointsUndistort.size()))
+    if (i < 0 || i >= static_cast<int>(m_keypoints.size()) || i >= static_cast<int>(m_keypointsUndistort.size())) {
+        std::cerr << "keypoint index " << i << " out of range" << std::endl;
         return false;
+    }
     m_keypoints[i].setClassId(classId);
     m_keypointsUndistort[i].setClassId(classId);
     return true;
