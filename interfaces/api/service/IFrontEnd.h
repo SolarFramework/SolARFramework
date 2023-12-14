5b4939d0d1037b4fa9e3fdb1de76c7a86b10ab2e
Christophe Cutullic <christophe.cutullic@b-com.com>
2023-12-14 18:15:47 +0100

    Fix (PR): go back from "enum class" to "typedef enum" to avoid too many impacts on other projects

--- interfaces/api/service/IFrontEnd.h
+++ interfaces/api/service/IFrontEnd.h
@@ -37,13 +37,13 @@ namespace service {
  * @typedef DeviceType
  * @brief <B>Define the types of all devices.</B>
  */
-enum class DeviceType {
+typedef enum {
     OTHER_DEVICE = 0,
     HOLOLENS2_HEADSET = 1,
     LYNX_HEADSET = 2,
     ANDROID_DEVICE = 3,
     IOS_DEVICE = 4
-};
+} DeviceType;
 
 /**
  * @struct DeviceInfo
