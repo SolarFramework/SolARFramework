aa3473e94692bcee9151202f26edd26f1aaf8510
Yitian Zhou <Yitian.Zhou@b-com.com>
2024-03-01 17:12:11 +0100

    Add output commonKeyframeIds to map fusion merge()

--- interfaces/api/solver/map/IMapFusion.h
+++ interfaces/api/solver/map/IMapFusion.h
@@ -48,12 +48,14 @@ public:
 	/// @param[in,out] map local map or floating map to merge
 	/// @param[in,out] globalMap the global map
 	/// @param[in,out] transform the transformation to the global map (null for floating map). It can be refined by fusion process.
+	/// @param[out] commonKeyframeIds list of IDs of the shared common keyframes between map and globalMap. Usually the map is extended from an initial submap of globalMap (the same submap exists in both map and globalMap), so the list is not empty. Otherwise will get an empty list.
 	/// @param[out] nbMatches the number of matched cloud points.
     /// @param[out] error the error of fusion process that is the mean of error distances of the matched cloud points.
 	/// @return FrameworkReturnCode::_SUCCESS_ if the fusion succeed, else FrameworkReturnCode::_ERROR.
     virtual FrameworkReturnCode merge(SRef<SolAR::datastructure::Map> map,
                                     SRef<SolAR::datastructure::Map> globalMap,
 									SolAR::datastructure::Transform3Df &transform, 
+									std::vector<uint32_t>& commonKeyframeIds,
 									uint32_t &nbMatches, 
 									float &error) = 0;
 
@@ -61,12 +63,14 @@ public:
 	/// @param[in,out] map local map or floating map to merge
 	/// @param[in,out] globalMap the global map	
 	/// @param[in,out] transform the transformation to the global map (null for floating map). It can be refined by fusion process.
+	/// @param[out] commonKeyframeIds list of IDs of the shared common keyframes between map and globalMap. Usually the map is extended from an initial submap of globalMap (the same submap exist in both map and globalMap), so the list is not empty. Otherwise will get an empty list.
 	/// @param[in] cpOverlapIndices pairs of detected overlap cloud points indices of floating map and global map.
 	/// @param[in] isRefineTransform refine the 3D transformation if it's true.
 	/// @return FrameworkReturnCode::_SUCCESS_ if the fusion succeed, else FrameworkReturnCode::_ERROR.
     virtual FrameworkReturnCode merge(SRef<SolAR::datastructure::Map> map,
                                     SRef<SolAR::datastructure::Map> globalMap,
 									SolAR::datastructure::Transform3Df &transform, 
+									std::vector<uint32_t>& commonKeyframeIds,
 									const std::vector<std::pair<uint32_t, uint32_t>>&cpOverlapIndices,
                                     bool isRefineTransform = false) = 0;
 };
