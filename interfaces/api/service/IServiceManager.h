5cd3e2f4c01c709b83b8cf6bd509c2430c68fb61
Christophe Cutullic <christophe.cutullic@b-com.com>
2024-09-05 17:40:42 +0200

    feat (interface): update Service Manager interface

--- interfaces/api/service/IServiceManager.h
+++ interfaces/api/service/IServiceManager.h
@@ -107,25 +107,26 @@ public:
     ///         FrameworkReturnCode::_ERROR_ for other errors
     virtual FrameworkReturnCode getService(const ServiceType serviceType, std::string & serviceURL) const = 0;
 
-    /// @brief Get an available URL for a specific service type, and lock it for the given client UUID
-    ///        If a service of the specific type is already locked for the given client UUID, return its URL
+    /// @brief Get an available URL for a specific service type, and lock it for the given client or map UUID
+    ///        If a service of the specific type is already locked for the given UUID, return its URL
     /// @param[in] serviceType type of the service
-    /// @param[in] clientUUID UUID of the client
+    /// @param[in] uuid UUID of the client or the map
     /// @param[out] serviceURL URL of the service
     /// @return FrameworkReturnCode::_SUCCESS if a service URL is available and the service locked, else
     ///         FrameworkReturnCode::_NO_SERVICE_REGISTERED if no service of the given type is registered
     ///         FrameworkReturnCode::_NO_SERVICE_AVAILABLE if no service of the given type is available
     ///         FrameworkReturnCode::_ERROR_ for other errors
-    virtual FrameworkReturnCode getAndLockService(const ServiceType serviceType, const std::string & clientUUID,
+    virtual FrameworkReturnCode getAndLockService(const ServiceType serviceType,
+                                                  const std::string & uuid,
                                                   std::string & serviceURL) = 0;
 
-    /// @brief Unlock the service of the given type, for the given client UUID
+    /// @brief Unlock the service of the given type, for the given client or map UUID
     /// @param[in] serviceType type of the service
-    /// @param[in] clientUUID UUID of the client
+    /// @param[in] uuid UUID of the client or the map
     /// @return FrameworkReturnCode::_SUCCESS if the service is unlocked, else
-    ///         FrameworkReturnCode::_NO_SERVICE_LOCKED if no service is locked for the client UUID
+    ///         FrameworkReturnCode::_NO_SERVICE_LOCKED if no service is locked for the UUID
     ///         FrameworkReturnCode::_ERROR_ for other errors
-    virtual FrameworkReturnCode unlockService(const ServiceType serviceType, const std::string & clientUUID) = 0;
+    virtual FrameworkReturnCode unlockService(const ServiceType serviceType, const std::string & uuid) = 0;
 };
 
 } // service
