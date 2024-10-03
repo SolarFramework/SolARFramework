aba73fe3aed6cff8e39ed3ffaa1cff5b79150fd0
jroyan <jerome.royan@b-com.com>
2024-10-03 09:15:12 +0200

    add interface to save, load and delete a world graph file

--- interfaces/api/storage/IWorldGraphManager.h
+++ interfaces/api/storage/IWorldGraphManager.h
@@ -203,6 +203,18 @@ public:
     /// @return a FrameworkReturnCode succesfull if the world link is correctly modified
     virtual FrameworkReturnCode modifyWorldLink(org::bcom::xpcf::uuids::uuid& id, SRef<datastructure::StorageWorldLink> worldLink) = 0;
 
+    /// @brief Save the map to the external file
+    /// @return FrameworkReturnCode::_SUCCESS_ if the backup succeeds, else FrameworkReturnCode::_ERROR.
+    virtual FrameworkReturnCode saveToFile() const = 0;
+
+    /// @brief Load the map from the external file
+    /// @return FrameworkReturnCode::_SUCCESS_ if the loading succeeds, else FrameworkReturnCode::_ERROR.
+    virtual FrameworkReturnCode loadFromFile() = 0;
+
+    /// @brief Delete the map in external file
+    /// @return FrameworkReturnCode::_SUCCESS_ if the deletion succeeds, else FrameworkReturnCode::_ERROR.
+    virtual FrameworkReturnCode deleteFile() = 0;
+
 
 };
 }
