de8a09c1365b6c5e95e020120f3727a26ad5abe8
ccutullic <christophe.cutullic@b-com.com>
2025-09-23 15:00:58 +0200

    fix (interface): error in getMapInfo method definition

--- interfaces/api/service/IFrontEnd.h
+++ interfaces/api/service/IFrontEnd.h
@@ -393,7 +393,7 @@ public:
     virtual FrameworkReturnCode getMapInfo(const std::string & accessToken,
                                            const std::string & mapUUID,
                                            std::string & version,
-                                           datastructure::GlobalDescriptorType & globalDescriptorType
+                                           datastructure::GlobalDescriptorType & globalDescriptorType,
                                            datastructure::DescriptorType & descriptorType,
                                            uint32_t & dataSize,
                                            bool & areImageSaved) const = 0;
