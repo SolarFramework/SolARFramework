de61e3b2831e5b72e21d514671f80b9957e09a57
Yitian.Zhou <yitian.zhou@b-com.com>
2026-09-16 16:31:55 +0200

    refactor: reduce number of methods in IRectifyMap

--- interfaces/api/map/IRectifyMap.h
+++ interfaces/api/map/IRectifyMap.h
@@ -40,7 +40,12 @@ public:
     /// @enum class RectifyMapProcessingStatus
     /// @brief define the different status of processing
     enum class RectifyMapProcessingStatus: std::underlying_type_t<ProcessingStatus> {
-        RUNNING_DESCRIPTOR_MATCHING = 5,
+        NOT_DEFINED,
+        NOT_INITIALIZED,
+        IDLE_INITIALIZED,
+        IDLE_COMPLETED,
+        IDLE_ABORTED,
+        RUNNING_DESCRIPTOR_MATCHING,
         IDLE_DESCRIPTOR_MATCHING_FINISHED,
         RUNNING_INITIAL_MAPPING,
         IDLE_INITIAL_MAPPING_FINISHED,
@@ -52,6 +57,11 @@ public:
     /// @brief return a string value of a ProcessingStatus value
     std::string toString(ProcessingStatus status) final {
         switch (static_cast<RectifyMapProcessingStatus>(status)) {
+            case RectifyMapProcessingStatus::NOT_DEFINED: return "NOT_DEFINED";
+            case RectifyMapProcessingStatus::NOT_INITIALIZED: return "NOT_INITIALIZED";
+            case RectifyMapProcessingStatus::IDLE_INITIALIZED: return "IDLE_INITIALIZED";
+            case RectifyMapProcessingStatus::IDLE_COMPLETED: return "IDLE_COMPLETED";
+            case RectifyMapProcessingStatus::IDLE_ABORTED: return "IDLE_ABORTED";
             case RectifyMapProcessingStatus::RUNNING_DESCRIPTOR_MATCHING: return "RUNNING_DESCRIPTOR_MATCHING";
             case RectifyMapProcessingStatus::IDLE_DESCRIPTOR_MATCHING_FINISHED: return "IDLE_DESCRIPTOR_MATCHING_FINISHED";
             case RectifyMapProcessingStatus::RUNNING_INITIAL_MAPPING: return "RUNNING_INITIAL_MAPPING";
@@ -63,67 +73,20 @@ public:
         }
     }
 
-public:
-
     ///@brief IRectifyMap default constructor.
     IRectifyMap() = default;
 
     ///@brief IRectifyMap default destructor.
     virtual ~IRectifyMap() override = default;
 
-    /// @brief Create a new map resulting from the processing of the original map
-    /// @param[in] map the original map
-    /// @return FrameworkReturnCode::_SUCCESS if the processing succeed, else FrameworkReturnCode::_ERROR_
-    /// @note This method is not applicable for this interface
-    FrameworkReturnCode createMap(const SRef<SolAR::datastructure::Map>& map) final;
+    /// @brief use the covisibility graph of input map during the map rectification
+    void useCovisibilityGraphOn();
 
-    /// @brief Create map from a set of images while camera parameters are not provided
-    /// @param[in] images list of images
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    virtual FrameworkReturnCode createMap(const std::vector<SRef<Image>>& images) = 0;
+    /// @brief do not use the covisibility graph of input map during the map rectification
+    void useCovisibilityGraphOff();
 
-    /// @brief Create map from a set of images with provided camera parameters
-    /// @param[in] imageCamIds list of pairs of image and camera ID
-    /// @param[in] cameraParameters list of camera parameters
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    virtual FrameworkReturnCode createMap(const std::vector<std::pair<SRef<Image>, uint32_t>>& imageCamIds, const std::vector<SRef<CameraParameters>>& cameraParameters) = 0;
-
-    /// @brief Create map from a set of keyframes and camera parameters
-    /// @param[in] keyframes list of keyframes
-    /// @param[in] cameraParameters list of camera parameters
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    virtual FrameworkReturnCode createMap(const std::vector<SRef<Keyframe>>& keyframes, const std::vector<SRef<CameraParameters>>& cameraParameters) = 0;
-
-    /// @brief Create map from a set of keyframes, camera parameters and covisibility graph
-    /// @param[in] keyframes list of keyframes
-    /// @param[in] cameraParameters list of camera parameters
-    /// @param[in] covGraph covisibility graph
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    virtual FrameworkReturnCode createMap(const std::vector<SRef<Keyframe>>& keyframes, const std::vector<SRef<CameraParameters>>& cameraParameters, const CovisibilityGraph& covGraph) = 0;
-
-    /// @brief Create map from a set of images with provided camera parameters
-    /// @param[in] imageCamIds list of pairs of image and camera ID
-    /// @param[in] cameraParameters collection of camera parameters
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    FrameworkReturnCode createMap(const std::vector<std::pair<SRef<Image>, uint32_t>>& imageCamIds, const SRef<CameraParametersCollection> cameraParameters);
-
-    /// @brief Create map from a set of keyframes and camera parameters
-    /// @param[in] keyframes collection of keyframes
-    /// @param[in] cameraParameters collection of camera parameters
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    FrameworkReturnCode createMap(const SRef<KeyframeCollection> keyframes, const SRef<CameraParametersCollection> cameraParameters);
-
-    /// @brief Create map from a set of keyframes and camera parameters
-    /// @param[in] keyframes list of keyframes
-    /// @param[in] cameraParameters collection of camera parameters
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    FrameworkReturnCode createMap(const std::vector<SRef<Keyframe>>& keyframes, const SRef<CameraParametersCollection> cameraParameters);
-
-    /// @brief Create map from a set of keyframes and camera parameters
-    /// @param[in] keyframes collection of keyframes
-    /// @param[in] cameraParameters list of camera parameters
-    /// @return FrameworkReturnCode::_SUCCESS if map is created successfully, otherwise FrameworkReturnCode::_ERROR_
-    FrameworkReturnCode createMap(const SRef<KeyframeCollection> keyframes, const std::vector<SRef<CameraParameters>>& cameraParameters);
+protected:
+    bool m_useCovisibilityGraph = false;
 
 };
 
